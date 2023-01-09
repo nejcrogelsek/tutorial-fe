56fb289670e48e7b2a4048edb67753aa8f40a5fa
nejcrogelsek <nejcrogelsek0@gmail.com>
2023-01-09 21:28:40 +0100

    Format code

--- src/components/user/CreateUpdateUserForm.tsx
+++ src/components/user/CreateUpdateUserForm.tsx
@@ -4,7 +4,7 @@ import {
   useCreateUpdateUserForm,
 } from 'hooks/react-hook-form/useCreateUpdateUser'
 import { ChangeEvent, FC, useEffect, useState } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import ToastContainer from 'react-bootstrap/ToastContainer'
 import Toast from 'react-bootstrap/Toast'
 import { Form } from 'react-bootstrap'
